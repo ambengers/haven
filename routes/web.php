b5645ae2fd66399ea945ccd470140d7cda624bcb
Marvin Quezon <quezon.marvin@gmail.com>
2020-12-22 06:03:55 +0800

    Fixes failing test.

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::group(['middleware' => ['auth']], function () {
+    require __DIR__.'/web/situations.php';
+});
+
 require __DIR__.'/auth.php';
