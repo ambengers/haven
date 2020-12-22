70227ed41d05d46d91481d203e1138640370f69c
Marvin Quezon <quezon.marvin@gmail.com>
2020-12-22 08:25:22 +0800

    Adds dashboard on sidebar.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,6 +2,19 @@
     <ul class="space-y-2 text-sm">
         <li>
             <a
+                href="#"
+                class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-indigo-200 focus:shadow-outline"
+            >
+                <span class="text-gray-600">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+                    </svg>
+                </span>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a
                 href="{{ route('situations.index') }}"
                 class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-indigo-200 focus:shadow-outline {{ request()->routeIs('situations.*') ? 'bg-indigo-200' : '' }}"
             >
