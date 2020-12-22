8e4caca889b17d0672fb1b9350589a3f91235aae
Marvin Quezon <quezon.marvin@gmail.com>
2020-12-22 08:22:31 +0800

    Adds sidebar items.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -13,5 +13,47 @@
                 <span>Situations</span>
             </a>
         </li>
+
+        <li>
+            <a
+                href="#"
+                class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-indigo-200 focus:shadow-outline"
+            >
+                <span class="text-gray-600">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
+                </span>
+                <span>Evacuation Centers</span>
+            </a>
+        </li>
+
+        <li>
+            <a
+                href="#"
+                class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-indigo-200 focus:shadow-outline"
+            >
+                <span class="text-gray-600">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
+                    </svg>
+                </span>
+                <span>Registry</span>
+            </a>
+        </li>
+
+        <li>
+            <a
+                href="#"
+                class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-indigo-200 focus:shadow-outline"
+            >
+                <span class="text-gray-600">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+                    </svg>
+                </span>
+                <span>User Management</span>
+            </a>
+        </li>
     </ul>
 </div>
